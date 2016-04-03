4226d09757cae7edc4b9fcea8220c30bc8485eb2
cylic-reference <alex@cyclicreference.space>
2016-04-03 13:05:46 -0500

    Bean Inheritance.

---
+++ src/main/java/space/cyclic/reference/ASDFReferences.java
@@ -0,0 +1,39 @@
+package space.cyclic.reference;
+
+public class ASDFReferences {
+    private String referenceOne;
+    private String referenceTwo;
+    private String referenceThree;
+
+    public ASDFReferences(){}
+
+    public ASDFReferences(String referenceOne, String referenceTwo , String referenceThree){
+        this.referenceOne = referenceOne;
+        this.referenceTwo = referenceTwo;
+        this.referenceThree = referenceThree;
+    }
+
+    public String getReferenceOne() {
+        return referenceOne;
+    }
+
+    public void setReferenceOne(String referenceOne) {
+        this.referenceOne = referenceOne;
+    }
+
+    public String getReferenceTwo() {
+        return referenceTwo;
+    }
+
+    public void setReferenceTwo(String referenceTwo) {
+        this.referenceTwo = referenceTwo;
+    }
+
+    public String getReferenceThree() {
+        return referenceThree;
+    }
+
+    public void setReferenceThree(String referenceThree) {
+        this.referenceThree = referenceThree;
+    }
+}
--- src/main/java/space/cyclic/reference/TrainingBeanPostProcess.java
+++ src/main/java/space/cyclic/reference/TrainingBeanPostProcess.java
@@ -3,6 +3,10 @@ package space.cyclic.reference;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class TrainingBeanPostProcess implements BeanPostProcessor{
     @Override
     public Object postProcessBeforeInitialization(Object stringBean, String stringBeanName) throws BeansException {
@@ -12,7 +16,16 @@ public class TrainingBeanPostProcess implements BeanPostProcessor{
 
     @Override
     public Object postProcessAfterInitialization(Object stringBean, String stringBeanName) throws BeansException {
-        System.out.println("TrainingBeanPostProcess.postProcessBeforeInitialization: After Init for bean : " + stringBeanName);
+        for (Method method : stringBean.getClass().getDeclaredMethods()){
+            if (Modifier.isPublic(method.getModifiers()) && !method.getReturnType().equals(Void.TYPE)){
+                try {
+                    System.out.println("TrainingBeanPostProcess.postProcessAfterInitialization: Bean \"" + stringBeanName +
+                    "\" property " + method.getName().substring(3) + ": " + method.invoke(stringBean));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    System.err.println(e.getLocalizedMessage());
+                }
+            }
+        }
         return stringBean;
     }
 }
